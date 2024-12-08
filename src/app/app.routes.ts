c97f6ac5a6e173fabe1e52ee9de01befb3b2d659
Bryan Vespignani <palma99@icloud.com>
2024-12-08 12:24:46 +0100

    feat: breadcrumb

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,20 +3,14 @@ import { AppComponent } from './app.component';
 import { authRoutes } from './core/auth/auth.routes';
 import { dashboardRoutes } from './features/dashboard/dashboard.routes';
 import { AdminLayoutComponent } from './core/layout/admin-layout/admin-layout.component';
+import { authGuard } from './core/auth/guards/auth.guard';
 
 export const routes: Routes = [
+  ...dashboardRoutes,
+  ...authRoutes,
   {
     path: '', component: AppComponent
   },
-
-  {
-    path: '', component: AdminLayoutComponent,
-    children: [
-      ...dashboardRoutes,
-    ]
-  },
-
-  ...authRoutes,
   {
     path: '**', redirectTo: ''
   }
--- src/app/features/dashboard/dashboard.routes.ts
+++ src/app/features/dashboard/dashboard.routes.ts
@@ -4,10 +4,39 @@ import { authGuard } from '../../core/auth/guards/auth.guard';
 export const dashboardRoutes: Routes = [
   {
     path: 'dashboard',
-    loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent),
     canActivate: [authGuard],
-    data: {
-      title: 'Dashboard'
-    }
+    loadComponent: () => import('../../core/layout/admin-layout/admin-layout.component').then(m => m.AdminLayoutComponent),
+    children: [
+      {
+        path: 'projects',
+        pathMatch: 'full',
+        loadComponent: () => import('./pages/projects/projects.component').then(m => m.ProjectsComponent), 
+        data: {
+          title: 'Projects'
+        }
+      },
+      {
+        path: '',
+        loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent),
+        data: {
+          title: 'Dashboard'
+        }
+      },
+    ]
   },
+
+  // {
+  //   path: 'dashboard',
+  //   loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent),
+  //   data: {
+  //     title: 'Dashboard'
+  //   }
+  // },
+  // {
+  //   path: 'projects',
+  //   loadComponent: () => import('./pages/projects/projects.component').then(m => m.ProjectsComponent), 
+  //   data: {
+  //     title: 'Projects'
+  //   }
+  // },
 ];
