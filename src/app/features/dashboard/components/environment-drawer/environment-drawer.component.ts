f593b15277a308978b1f0b090296edd53270e85e
Bryan Vespignani <palma99@icloud.com>
2024-12-11 21:38:41 +0100

    feat: refactor flag list in separate component

--- src/app/features/dashboard/components/environment-drawer/environment-drawer.component.ts
+++ src/app/features/dashboard/components/environment-drawer/environment-drawer.component.ts
@@ -1,112 +1,39 @@
-import { Component, computed, inject, linkedSignal } from '@angular/core';
-import { TuiAlertService, TuiButton, TuiPopup } from '@taiga-ui/core';
-import { TuiButtonLoading, TuiDrawer, TuiSwitch } from '@taiga-ui/kit';
+import { Component, inject } from '@angular/core';
+import { TuiButton, TuiPopup } from '@taiga-ui/core';
+import { TuiDrawer } from '@taiga-ui/kit';
 import { EnvironmentDrawerService } from './environment-drawer.service';
-import { TuiCell, TuiHeader } from '@taiga-ui/layout';
+import { TuiHeader } from '@taiga-ui/layout';
 import { rxResource } from '@angular/core/rxjs-interop';
-import { ProjectsService } from '../../projects.service';
-import { Environment, EnvironmentDetails } from '../../models/Environment';
+import { EnvironmentDetails } from '../../models/Environment';
 import { ClipboardService } from '../../../../shared/clipboard.service';
 import { EnvironmentService } from '../../environment.service';
 import { NewFlagFormComponent } from "../new-flag-form/new-flag-form.component";
 import { FormsModule } from '@angular/forms';
-import { Flag } from '../../models/Flag';
-import { FlagService } from '../../flag.service';
-import { useRequestStatus } from '../../../../shared/use-request-status';
+import { FlagListComponent } from "./flag-list/flag-list.component";
 
 @Component({
   selector: 'app-environment-drawer',
   imports: [
     TuiPopup,
-    TuiButtonLoading,
     TuiDrawer,
     TuiButton,
     TuiDrawer,
     TuiHeader,
-    TuiCell,
-    TuiSwitch,
     FormsModule,
-    NewFlagFormComponent
+    NewFlagFormComponent,
+    FlagListComponent
 ],
   templateUrl: './environment-drawer.component.html',
   styleUrl: './environment-drawer.component.scss'
 })
 export class EnvironmentDrawerComponent {
   protected environmentDrawerService = inject(EnvironmentDrawerService)
-  private alerts = inject(TuiAlertService)
   private environmentService = inject(EnvironmentService)
-  private flagService = inject(FlagService)
 
   clipboardService = inject(ClipboardService)
-
-  updateFlagsRequestStatus = useRequestStatus()
 
   environmentResource = rxResource<EnvironmentDetails, number | null>({
     request: this.environmentDrawerService.selectedEnvironmentId,
     loader: ({ request: environmentId }) => this.environmentService.fetchEnvironmentDetails(environmentId)
   })
-
-  localModel = linkedSignal(this.environmentResource.value); 
-
-  sortedFlags = computed(() => this.localModel()?.flags?.sort((a, b) => a.name.localeCompare(b.name)) ?? [])
-
-  toggleLocalFlag(flagId: number) {
-    this.localModel.update((localEnv) => {
-      if (!localEnv) {
-        return localEnv
-      }
-
-      return {
-        ...localEnv,
-        flags: localEnv?.flags?.map((flag) => {
-          if (flag.id === flagId) {
-            return {
-              ...flag,
-              enabled: !flag.enabled
-            }
-          }
-          return flag
-        })
-      }
-    })
-  }
-
-  flagsToSave = computed<Flag[]>(() => {
-    return this.localModel()?.flags.filter((localFlag) => {
-      const remoteFlag = this.environmentResource.value()?.flags?.find((flag) => flag.id === localFlag.id);
-      if (!remoteFlag) {
-        return true
-      }
-      return localFlag.enabled !== remoteFlag.enabled
-    }) ?? []
-  })
-
-  changesToSave = computed(() => this.flagsToSave().length > 0)
-
-  saveChanges() {
-    this.updateFlagsRequestStatus.init()
-
-    this.flagService.updateFlags(this.environmentResource.value()!.id, this.flagsToSave())
-      .subscribe({
-        next: () => {
-          this.environmentResource.reload()
-          this.updateFlagsRequestStatus.loading.set(false)
-
-          this.alerts.open('Your changes have been saved!', {
-            appearance: 'positive',
-          }).subscribe()
-        },
-        error: (error) => {
-          this.alerts.open('There was an error saving your changes, please try again', {
-            appearance: 'negative',
-          }).subscribe()
-          this.updateFlagsRequestStatus.error.set(error.message)
-          this.updateFlagsRequestStatus.loading.set(false)
-        },
-      })
-  }
-
-  resetLocalFlags() {
-    this.localModel.set(this.environmentResource.value())
-  }
 }
---
+++ src/app/features/dashboard/components/environment-drawer/flag-list/flag-list.component.ts
@@ -0,0 +1,96 @@
+import { Component, computed, inject, input, linkedSignal, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TuiButton, TuiAlertService } from '@taiga-ui/core';
+import { TuiButtonLoading, TuiSwitch } from '@taiga-ui/kit';
+import { TuiCell } from '@taiga-ui/layout';
+import { ClipboardService } from '../../../../../shared/clipboard.service';
+import { useRequestStatus } from '../../../../../shared/use-request-status';
+import { FlagService } from '../../../flag.service';
+import { Flag } from '../../../models/Flag';
+import { EnvironmentDrawerService } from '../environment-drawer.service';
+
+@Component({
+  selector: 'app-flag-list',
+  imports: [
+    TuiButtonLoading,
+    TuiButton,
+    TuiCell,
+    TuiSwitch,
+    FormsModule,
+  ],
+  templateUrl: './flag-list.component.html',
+  styleUrl: './flag-list.component.scss'
+})
+export class FlagListComponent {
+  
+  remoteFlags = input<Flag[]>([])
+  environmentId = input<number>()
+  saved = output()
+
+  protected environmentDrawerService = inject(EnvironmentDrawerService)
+  private alerts = inject(TuiAlertService)
+  private flagService = inject(FlagService)
+
+  clipboardService = inject(ClipboardService)
+
+  updateFlagsRequestStatus = useRequestStatus()
+
+  localModel = linkedSignal(this.remoteFlags); 
+
+  sortedFlags = computed(() => this.localModel().sort((a, b) => a.name.localeCompare(b.name)) ?? [])
+
+  toggleLocalFlag(flagId: number) {
+    this.localModel.update((localEnv) => {
+      return localEnv.map((flag) => {
+        if (flag.id === flagId) {
+          return {
+            ...flag,
+            enabled: !flag.enabled
+          }
+        }
+        return flag
+      })
+    })
+  }
+
+  flagsToSave = computed<Flag[]>(() => {
+    return this.localModel().filter((localFlag) => {
+      const remoteFlag = this.remoteFlags().find((flag) => flag.id === localFlag.id);
+      if (!remoteFlag) {
+        return true
+      }
+      return localFlag.enabled !== remoteFlag.enabled
+    }) ?? []
+  })
+
+  changesToSave = computed(() => this.flagsToSave().length > 0)
+
+  saveChanges() {
+    this.updateFlagsRequestStatus.init()
+    if (this.environmentId() === null) {
+      return
+    }
+
+    this.flagService.updateFlags(this.environmentId()!, this.flagsToSave())
+      .subscribe({
+        next: () => {
+          this.updateFlagsRequestStatus.loading.set(false)
+          this.saved.emit()
+          this.alerts.open('Your changes have been saved!', {
+            appearance: 'positive',
+          }).subscribe()
+        },
+        error: (error) => {
+          this.alerts.open('There was an error saving your changes, please try again', {
+            appearance: 'negative',
+          }).subscribe()
+          this.updateFlagsRequestStatus.error.set(error.message)
+          this.updateFlagsRequestStatus.loading.set(false)
+        },
+      })
+  }
+
+  resetLocalFlags() {
+    this.localModel.set(this.remoteFlags())
+  }
+}
