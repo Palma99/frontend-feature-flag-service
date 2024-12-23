cc77c000c346f994d6b9af24080b977d6d624073
Bryan Vespignani <palma99@icloud.com>
2024-12-23 22:10:18 +0100

    projects link in dashboard

--- src/app/features/dashboard/pages/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TuiAppearance, TuiLink, TuiTitle } from '@taiga-ui/core';
+import { TuiCardMedium } from '@taiga-ui/layout';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    TuiAppearance,
+    TuiTitle,
+    RouterLink,
+    TuiLink,
+    TuiCardMedium,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
-
 }
